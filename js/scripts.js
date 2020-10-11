3605e103e93f9c9701f7263b0f6501c1c78b52df
BenardBett <kiplangatbett5@gmail.com>
2020-10-11 10:06:02 +0300

    added another div and a function in js file

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,14 @@ $(document).ready(function(){
       $("#design-icon").slideDown('1500');
     });
   });
+
+  $(document).ready(function(){
+    $("#development-icon").click(function(){
+      $("#development-icon").slideDown('1500').hide('1000');
+      $("#development").show('1500');
+    });
+    $("#development").click(function(){
+      $("#development").slideUp('1500');
+      $("#development-icon").slideDown('1500');
+    });
+  });
