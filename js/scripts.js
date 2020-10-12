a85f4772b628baed0aba58dd1fe6f195588b5eaa
BenardBett <kiplangatbett5@gmail.com>
2020-10-12 17:21:09 +0300

    modified js file

--- js/scripts.js
+++ js/scripts.js
@@ -93,13 +93,13 @@ $(document).ready(function(){
       $(".port8").hide()
     })
   })
-  function submit(){
+  $("button").click(function(){
     var name=document.getElementById("name").value;
     var messages = document.getElementById("message").value;
-    if(name,messages == ''){
+    if(name,messages === ""){
         alert("Make sure all field are filled")
     }
     else{
-    alert("Thank you for contacting use. we hae recived your message" + ' ' + name );
+    alert("Thank you for reaching out to us. We have received your message" + ' ' + name );
     }
-    };
+    });
